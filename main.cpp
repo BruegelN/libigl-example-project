b0e8ff3fb756ab3b9fbb85236450019ee0108da6
Nico Brügel <Nico.Bruegel@htwg-konstanz.de>
2019-09-01 17:05:50 +0200

    code from issue: https://github.com/libigl/libigl/issues/1283

--- main.cpp
+++ main.cpp
@@ -1,34 +1,41 @@
-#include <igl/opengl/glfw/Viewer.h>
+#include <igl/embree/line_mesh_intersection.h>
+#include <igl/readSTL.h>
 
 int main(int argc, char *argv[])
 {
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    1,7,5,
-    1,3,7,
-    1,4,3,
-    1,2,4,
-    3,8,7,
-    3,4,8,
-    5,7,8,
-    5,8,6,
-    1,5,6,
-    1,6,2,
-    2,6,8,
-    2,8,4).finished().array()-1;
 
-  // Plot the mesh
-  igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
-  viewer.launch();
+
+  
+  std::vector<std::vector > V;
+  std::vector<std::vector > F;
+  std::vector<std::vector > N;
+  bool success = igl::readSTL("File_path.stl",V,F,N);
+  Eigen::MatrixXd V_1(V.size(),3);
+  Eigen::MatrixXi F_1(F.size(),3);
+  Eigen::MatrixXd N_1(N.size(),3);
+  Eigen::MatrixXd V_1_f(1,3);
+  Eigen::MatrixXd N_1_f(1,3);
+  V_1_f(0,0)=rNode.X(); V_1_f(0,1)=rNode.Y(); V_1_f(0,2)=rNode.Z();
+  N_1_f(0,0)=rRayVector[0]; N_1_f(0,1)=rRayVector[1]; N_1_f(0,2)=rRayVector[2];
+
+for (int i=0; i<V.size(); ++i){
+
+  V_1(i,0)=V[i][0]; V_1(i,1)=V[i][1]; V_1(i,2)=V[i][2];
+  //std::cout<<V_1(i,0)<<V_1(i,1)<<V_1(i,2)<<std::endl;
+
 }
+for (int i=0; i<F.size(); ++i){
+
+  F_1(i,0)=F[i][0]; F_1(i,1)=F[i][1]; F_1(i,2)=F[i][2];
+  //std::cout<<F_1(i,0)<<F_1(i,1)<<F_1(i,2)<<std::endl;
+
+}
+for (int i=0; i<N.size(); ++i){
+
+  N_1(i,0)=N[i][0]; N_1(i,1)=N[i][1]; N_1(i,2)=N[i][2];
+  //std::cout<<N_1(i,0)<<N_1(i,1)<<N_1(i,2)<<std::endl;
+
+}
+
+Eigen::MatrixXd R = igl::embree::line_mesh_intersection<Eigen::MatrixXd, Eigen::MatrixXi >(V_1_f, N_1_f, V_1, F_1);
+std::cout<<"Ray Hit !!!!!!!!!!!!!!!!!!"<<R<<std::endl;}
